12ee3501c5d1524dbaddfd9f736db064d62aaad8
Albinus <99928204+albibos@users.noreply.github.com>
2023-05-06 21:59:23 -0400

    Update library.js

--- library.js
+++ library.js
@@ -14,6 +14,28 @@ var Gallium = {
     f.style.border = 'none';
     f.style.margin = '0';
     win.document.body.style.height = '100vh';
+  },
+  blob: function(url) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.responseType = 'blob';
+
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var blobUrl = URL.createObjectURL(xhr.response);
+          resolve(blobUrl);
+        } else {
+          reject(new Error('Failed to convert URL to blob.'));
+        }
+      };
+
+      xhr.onerror = function() {
+        reject(new Error('Failed to convert URL to blob.'));
+      };
+
+      xhr.send();
+    });
   }
 };
 
