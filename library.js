f07b0101c77d2f40692fe0e111f85baefd6d74c8
Albinus <99928204+albibos@users.noreply.github.com>
2023-05-06 22:10:42 -0400

    Update library.js

--- library.js
+++ library.js
@@ -16,7 +16,12 @@ var Gallium = {
     win.document.body.style.height = '100vh';
   },
   blob: function(url) {
-    var page = new Blob([`<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="${url}"></iframe>`], { type: "text/html" });
+    var page = new Blob(
+      [
+        `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="${url}"></iframe>`,
+      ],
+      { type: "text/html" }
+    );
     window.open(URL.createObjectURL(page));
   }
 };
